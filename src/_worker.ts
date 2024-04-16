63739eddbe509db6a4facf63267bab14822ddff2
Aaron Croissette <aaron@kinetech.llc>
2024-04-16 02:29:33 -0400

    refactor: cleanup

--- src/_worker.ts
+++ src/_worker.ts
@@ -2,7 +2,7 @@ import app from "./app";
 
 export default {
   async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
-    console.log(ctx);
+    // console.log(ctx);
     return app(env).fetch(request);
     // ctx.waitUntil(client.end());
   }
